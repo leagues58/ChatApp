94035acdb5292d1cd17bdd44c9b678d31f1e91fc
Eric <leagues58@gmail.com>
2019-10-05 21:07:45 -0400

    Added route for index, and first socketio code for new connections

--- server/index.js
+++ server/index.js
@@ -4,8 +4,22 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+  console.log(`There is a new connection`);
+  
+  socket.on('disconnect', () => {
+    console.log('User has disconnected');
+  });
+});
+
+
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
