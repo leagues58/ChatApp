fbc7a010f6c965d3b9341375df73655cc067577e
Eric <leagues58@gmail.com>
2019-10-06 12:17:26 -0400

    Added info bar for online status to top of chat page

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import InfoBar from './../InfoBar/InfoBar.js';
+import './chat.css';
+
 let socket;
+
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
@@ -50,6 +54,7 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
+        <InfoBar room={room} />
         <input value={message} onChange={(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null} />
       </div>
     </div>
